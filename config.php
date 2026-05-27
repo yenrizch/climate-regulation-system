ef33ffc51d2b3c3987b337be21ef28726f3a7b19
yenrizch <yenrizch@gmail.com>
2026-05-27 13:06:02 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ $user = "root";
 $pass = getenv("DB_PASS");
 $db   = "railway";
 $port = "29496";
-
 try {
     $conn = new PDO(
         "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
@@ -15,11 +14,4 @@ try {
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
-
-// ✏️ CHANGE THESE 3 LINES ONLY
-define('SMTP_HOST',   'smtp.gmail.com');
-define('SMTP_PORT',   587);
-define('SMTP_USER',   'rizcathnova@gmail.com'); // Gmail you just created
-define('SMTP_PASS',   'irtl odth axje shta');     // App Password from Part 2
-define('SMTP_NOTIFY', 'yenrizch@gmail.com'); // who receives the alert
 ?>
